a2985175187b38eb2a77e596a56dcd8661710287
Ronald Coello <ronaldcoello85@gmail.com>
2023-06-26 13:17:18 -0600

    added info dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -8,6 +8,18 @@ import { ReactSVG } from 'react-svg';
 
 export default function Dashboard() {
 
+    const account = [{
+        city: "Rexburg",
+        country: "USA",
+        dealership:"Lakeram Narine",
+        email: "nar19002@byui.edu",
+        firstName: "Lakeram",
+        lastName: "Narine",
+        phoneNumber: "2084775405",
+        region: "Alaska",
+        website: "www.honda.com"
+    }]
+    
     const car = [{make: "Honda", model:"Civic", year:"2019", description:"Reliable and fuel-efficient compact car."}]
     const selling = [{make: "Honda", model:"Civic", year:"2019", description:"Reliable and fuel-efficient compact car."}
     ]
@@ -21,20 +33,20 @@ export default function Dashboard() {
             <div className='dashboard__wrapper_col_1'>
                 <div className='dashboard__wrapper__profile'>
                     <div className='dashboard__wrapper__profile__picture'>
-                        RC
+                        {account[0].firstName.slice(0,1)}{account[0].lastName.slice(0,1)}
                     </div>
                     <div className='dashboard__wrapper__profile__info'>
-                        <h2>Ryan Smith</h2>
+                        <h2>{account[0].firstName} {account[0].lastName}</h2>
                         <h3>Contact Information</h3>
-                        <p>Ryan's car</p>
-                        <p>ryanscar@gmail.com</p>
-                        <p>www.ryancars.com</p>
-                        <p>+1 208 345 6789</p>
+                        <p>{account[0].dealership}</p>
+                        <p>{account[0].email}</p>
+                        <p>{account[0].website}</p>
+                        <p>{account[0].phoneNumber}</p>
 
                         <h3>Location</h3>
-                        <p>Idaho Falls</p>
-                        <p>Idaho</p>
-                        <p>USA</p>
+                        <p>{account[0].city}</p>
+                        <p>{account[0].region}</p>
+                        <p>{account[0].country}</p>
                     </div>
                     <ReactSVG src={menu} className='menu' onClick={()=>{setShowOptions(!showOptions)}}/>
                     {showOptions && <div className='options'>
