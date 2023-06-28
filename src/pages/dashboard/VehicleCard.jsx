398f96d3d28e14013ad41e768f516225896514cb
lakeram21 <leonarine@gmail.com>
2023-06-27 18:34:15 -0600

    outside click for the dropdown on the vechile side

--- src/pages/dashboard/VehicleCard.jsx
+++ src/pages/dashboard/VehicleCard.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../../styles/blocks/card.css';
-import menu from '../../images/menu_dots.svg'
+import menu from '../../images/menu_dots.svg';
 import { ReactSVG } from 'react-svg';
 
 export default function Card({ car, onClick, isSelected }) {
-    const [showOptions, setShowOptions] = useState(false)
+  const [showOptions, setShowOptions] = useState(false);
+  const optionsRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseDown = (event) => {
+      if (optionsRef.current && !optionsRef.current.contains(event.target)) {
+        setShowOptions(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleMouseDown);
+    return () => {
+      document.removeEventListener('mousedown', handleMouseDown);
+    };
+  }, []);
 
   return (
     <div className='vehiclecard' onClick={onClick}>
@@ -12,23 +26,24 @@ export default function Card({ car, onClick, isSelected }) {
         <h3>{car.year} {car.make} {car.model}</h3>
 
         <div className='vehiclecard__title__options'>
-            <ReactSVG src={menu} className='menu' onClick={()=>{setShowOptions(!showOptions)}}/>
-            {showOptions && <div className='options'>
-                <ul>
-                    <li><a>Edit</a></li>
-                    <li><a>Mark as Found</a></li>
-                    <li><a>Delete</a></li>
-                </ul>
+          <ReactSVG src={menu} className='menu' onClick={() => { setShowOptions(!showOptions) }} />
+          {showOptions && (
+            <div ref={optionsRef} className='options'>
+              <ul>
+                <li><a>Edit</a></li>
+                <li><a>Mark as Found</a></li>
+                <li><a>Delete</a></li>
+              </ul>
             </div>
-            }
+          )}
         </div>
 
       </div>
-    
-        <div className='vehiclecard__description'>
-          {car.description}
-        </div>
-     
+
+      <div className='vehiclecard__description'>
+        {car.description}
+      </div>
+
     </div>
   );
 }
