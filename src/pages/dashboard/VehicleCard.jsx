7c59e9e0ac972a5dc81e12f61c6a296b437b6cb1
Ronald Coello <ronaldcoello85@gmail.com>
2023-07-11 01:29:18 -0600

    added edit

--- src/pages/dashboard/VehicleCard.jsx
+++ src/pages/dashboard/VehicleCard.jsx
@@ -4,6 +4,9 @@ import menu from "../../images/menu_dots.svg";
 import { ReactSVG } from "react-svg";
 import { updateInventoryRecord } from "../../Firebase/FirebaseStateManagement";
 import { Store } from "react-notifications-component";
+import DropDown from '../../shared/dropdown/DropDown';
+import { getVehicles } from '../../Firebase/FirebaseStateManagement';
+
 
 export default function Card({
   car,
@@ -15,6 +18,99 @@ export default function Card({
 }) {
   const [showOptions, setShowOptions] = useState(false);
   const optionsRef = useRef(null);
+
+
+  const [editMode, setEditMode] = useState(false);
+  const [updatedCarData, setUpdatedCarData] = useState({ ...car });
+
+
+  const [makeSelected, setMakeSelected] = useState(car.make)
+  const [modelSelected, setModelSelected] = useState(car.model)
+
+  // Data for Dropdown
+  const [dataVehicles, setDataVehicles] = useState(null)
+  const [makes, setMakes] = useState(undefined)
+  const [models, setModels] = useState(undefined)
+
+
+  // Get Static Data
+  const fetchdata=async()=>{
+    const vehicles = await getVehicles()
+    setDataVehicles(vehicles)
+        
+  }
+
+  useEffect(()=> {
+    if (dataVehicles) {
+      setMakes(Object.keys(dataVehicles[0]).sort())
+      setModels(dataVehicles[0][makeSelected])
+    }
+   
+  },[dataVehicles])
+
+  const handleCancel = () => {
+    setEditMode(!editMode);
+    setMakeSelected(car.make)
+   
+  }
+
+
+  useEffect(()=> {
+    setModelSelected(car.model)
+  },[editMode])
+
+  useEffect(()=> {
+    
+    if (dataVehicles) {
+     
+      setModels(dataVehicles[0][makeSelected])
+      setModelSelected("")
+    }
+    setUpdatedCarData({
+      ...updatedCarData,
+      make: makeSelected,
+    })
+    
+  },[makeSelected])
+
+  useEffect(()=> {
+   
+    if (dataVehicles) {
+      setModels(dataVehicles[0][makeSelected])
+    }
+    setUpdatedCarData({
+      ...updatedCarData,
+      model: modelSelected,
+    })
+  },[modelSelected])
+
+  useEffect(()=> {
+
+    fetchdata()
+  },[])
+
+  // This handles the car data update
+  const changeCarData =async () => {
+    await updateInventoryRecord(car.id, updatedCarData, type);
+    setEditMode(!editMode)
+    onUpdate(!update);
+
+
+    Store.addNotification({
+      title: "Success",
+      message: "Your vehicle was sucessfully updated",
+      type: "success",
+      insert: "top",
+      container: "top-right",
+      animationIn: ["animate__animated", "animate__fadeInDown"],
+      animationOut: ["animate__animated", "animate__fadeOut"],
+      dismiss: {
+        duration: 5000,
+        showIcon: true,
+      },
+    });
+  }
+
 
   // This Handle the Update on the Status as Sold or Found
   const changeStatus = async (type, car) => {
@@ -31,9 +127,7 @@ export default function Card({
         status: "Sold",
       };
     }
-    // Make the update
-    await updateInventoryRecord(car.id, updatedCar, type);
-    onUpdate(!update);
+    
 
     Store.addNotification({
       title: "Success",
@@ -93,23 +187,22 @@ export default function Card({
 
   return (
     <div className="vehiclecard" onClick={onClick}>
-      <div
-        className="vehiclecard__title"
-        onClick={() => {
-          setShowOptions(!showOptions);
-        }}
-      >
-        <h3>
-          {car.year} {car.make} {car.model}
-        </h3>
+      <div className="vehiclecard__title">
+        {!editMode &&
+          <h3>
+            {car.year} {car.make} {car.model}
+          </h3>
+        }
 
         <div className="vehiclecard__title__options">
-          <ReactSVG src={menu} className="menu" />
+          {!editMode && <ReactSVG src={menu} className="menu" onClick={() => {
+          setShowOptions(!showOptions);
+        }}/>}
           {showOptions && (
             <div ref={optionsRef} className="options">
               <ul>
                 <li>
-                  <a>Edit</a>
+                  <a onClick={()=> {setEditMode(!editMode);setShowOptions(!showOptions)}}>Edit</a>
                 </li>
                 {type == "listing" ? (
                   <li>
@@ -134,7 +227,63 @@ export default function Card({
         </div>
       </div>
 
-      <div className="vehiclecard__description">{car.description}</div>
+      {editMode? 
+        (
+        
+        <div className="vehiclecard__edit_car">
+            <label>
+              <p>Make</p>
+              <DropDown initial="Other" selectedOption={makeSelected} setSelectedOption={setMakeSelected} data={makes}/>
+
+
+              {makes && !makes.includes(makeSelected) &&makeSelected !== ''?
+              <input required style={{marginTop:'20px'}} type='text' placeholder='Type in the Make' onChange={(e)=>{setMakeSelected(e.target.value)}}/>             
+              :null
+              }
+
+            </label>
+
+            <label>
+              <p>Model</p>
+              {makes && !makes.includes(makeSelected)?
+              <input required  type='text' value={modelSelected} onChange={(e)=>{setModelSelected(e.target.value)}}/>             
+              : <DropDown initial="Other" selectedOption={modelSelected} setSelectedOption={setModelSelected} data={models}/>
+           
+              }
+            </label>
+
+            
+
+          <label>
+            <p>Year</p>
+            <input type="text" name="" value={updatedCarData.year} onChange={(e) =>
+                  setUpdatedCarData({
+                    ...updatedCarData,
+                    year: e.target.value,
+                  })
+                } />
+          </label>
+            <label>
+              <p>Description</p>
+              <textarea defaultValue={updatedCarData.description} name="" id="" cols="10" rows="10"  onChange={(e) =>
+                  setUpdatedCarData({
+                    ...updatedCarData,
+                    description: e.target.value,
+                  })
+                }>
+              </textarea>
+            </label>
+          <div style={{display:"flex",gap:"10px"}}>
+            <a onClick={()=> changeCarData()}>Update</a>
+            <a  className="vehiclecard__cancel" onClick={()=> handleCancel()}>Cancel</a>
+          </div>
+        </div>)  : 
+        <>
+          <div className="vehiclecard__description">{car.description}</div>
+          
+        </>
+      }
+      
     </div>
   );
 }
