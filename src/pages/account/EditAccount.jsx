bb5011d47f19181cb70fc73314d7985582f6555d
Ronald Coello <ronaldcoello85@gmail.com>
2023-07-15 13:07:35 -0600

    fix landing

--- src/pages/account/EditAccount.jsx
+++ src/pages/account/EditAccount.jsx
@@ -54,95 +54,99 @@ console.log(updatedUser)
   return (
     <div className='edit_account'>
       <div className='edit_account__wrapper'>
-        <h1>Edit account</h1>
-        <form onSubmit={updateUser}>
-          <label>
-            <p>First Name</p>
-            <input
-              type='text'
-              required
-              value={updatedUser.firstName || ""}
-              onChange={(e) => {
-                setUpdatedUser((prevUser) => ({
-                  ...prevUser,
-                  firstName: e.target.value,
-                }));
-              }}
-            />
-          </label>
-          <label>
-            <p>Last Name</p>
-            <input
-              type='text'
-              required
-              value={updatedUser.lastName || ""}
-              onChange={(e) => {
-                setUpdatedUser((prevUser) => ({
-                  ...prevUser,
-                  lastName: e.target.value,
-                }));
-              }}
-            />
-          </label>
-          <label>
-            <p>Email</p>
-            <input
-              type='email'
-              required
-              value={updatedUser.email || ""}
-              onChange={(e) => {
-                setUpdatedUser((prevUser) => ({
-                  ...prevUser,
-                  email: e.target.value,
-                }));
-              }}
-            />
-          </label>
-          <label>
-            <p>Business Name</p>
-            <input
-              type='text'
-              required
-              value={updatedUser.dealership || ""}
-              onChange={(e) => {
-                setUpdatedUser((prevUser) => ({
-                  ...prevUser,
-                  dealership: e.target.value,
-                }));
-              }}
-              minLength='2'
-            />
-          </label>
-          <label>
-            <p>Website</p>
-            <input
-              type='text'
-              required
-              value={updatedUser.website || ""}
-              onChange={(e) => {
-                setUpdatedUser((prevUser) => ({
-                  ...prevUser,
-                  website: e.target.value,
-                }));
-              }}
-            />
-          </label>
-          <label>
-            <p>Phone Number</p>
-            <input
-              type='tel'
-              value={updatedUser.phoneNumber || ""}
-              onChange={(e) => {
-                setUpdatedUser((prevUser) => ({
-                  ...prevUser,
-                  phoneNumber: e.target.value,
-                }));
-              }}
-              required
-            />
-          </label>
-          <button type="submit">Update Account</button>
-        </form>
+        <div className='edit_account__wrapper__description'>
+          <h1>Edit account</h1>
+        </div>
+        <div className="edit_account__wrapper__form">
+          <form onSubmit={updateUser}>
+            <label>
+              <p>First Name</p>
+              <input
+                type='text'
+                required
+                value={updatedUser.firstName || ""}
+                onChange={(e) => {
+                  setUpdatedUser((prevUser) => ({
+                    ...prevUser,
+                    firstName: e.target.value,
+                  }));
+                }}
+              />
+            </label>
+            <label>
+              <p>Last Name</p>
+              <input
+                type='text'
+                required
+                value={updatedUser.lastName || ""}
+                onChange={(e) => {
+                  setUpdatedUser((prevUser) => ({
+                    ...prevUser,
+                    lastName: e.target.value,
+                  }));
+                }}
+              />
+            </label>
+            <label>
+              <p>Email</p>
+              <input
+                type='email'
+                required
+                value={updatedUser.email || ""}
+                onChange={(e) => {
+                  setUpdatedUser((prevUser) => ({
+                    ...prevUser,
+                    email: e.target.value,
+                  }));
+                }}
+              />
+            </label>
+            <label>
+              <p>Business Name</p>
+              <input
+                type='text'
+                required
+                value={updatedUser.dealership || ""}
+                onChange={(e) => {
+                  setUpdatedUser((prevUser) => ({
+                    ...prevUser,
+                    dealership: e.target.value,
+                  }));
+                }}
+                minLength='2'
+              />
+            </label>
+            <label>
+              <p>Website</p>
+              <input
+                type='text'
+                required
+                value={updatedUser.website || ""}
+                onChange={(e) => {
+                  setUpdatedUser((prevUser) => ({
+                    ...prevUser,
+                    website: e.target.value,
+                  }));
+                }}
+              />
+            </label>
+            <label>
+              <p>Phone Number</p>
+              <input
+                type='tel'
+                value={updatedUser.phoneNumber || ""}
+                onChange={(e) => {
+                  setUpdatedUser((prevUser) => ({
+                    ...prevUser,
+                    phoneNumber: e.target.value,
+                  }));
+                }}
+                required
+              />
+            </label>
+            <button type="submit">Update Account</button>
+          </form>
+        </div>
       </div>
     </div>
   );
